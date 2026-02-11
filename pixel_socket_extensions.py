75e8ee956309e8353c88710bb9d0ea0e84c0215a
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2026-02-11 15:37:25 +0900

    clean: Renamed files

--- pixel_socket_extensions.py
+++ pixel_socket_extensions.py
@@ -2,10 +2,10 @@ from comfy_api.latest import ComfyExtension, io as comfy_api_io # pyright: ignor
 
 class PixelSocketExtensions(ComfyExtension):
     async def get_node_list(self) -> list[type[comfy_api_io.ComfyNode]]:
-        from .pixel_socket_node_delivery import PixelSocketDeliveryImageNode
-        from .pixel_socket_node_load_url import PixelSocketLoadImageFromUrlNode
-        from .pixel_socket_node_resize import PixelSocketResizeImageNode
-        from .pixel_socket_node_load_pnginfo import PixelSocketLoadImageInfoNode
+        from .pixel_socket_delivery import PixelSocketDeliveryImageNode
+        from .pixel_socket_load_url import PixelSocketLoadImageFromUrlNode
+        from .pixel_socket_resize import PixelSocketResizeImageNode
+        from .pixel_socket_load_pnginfo import PixelSocketLoadImageInfoNode
         return [
                     PixelSocketDeliveryImageNode,
                     PixelSocketLoadImageFromUrlNode,
